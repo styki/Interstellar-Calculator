7c67956a9be8f0c885154d5b005c6c618fd02011
Stanislav Shterev <stanislav@devlabs.bg>
2016-07-05 17:33:26 +0300

    Added max speed.

--- calculate.js
+++ calculate.js
@@ -1,16 +1,31 @@
 var lightYear = 9.461e+15;
 var year = 3.154e+7;
 var acceleration = 0.05;
-var distance = $("#input1");
-var time = $("#output1");
+var speedOfLight = 3e+8;
+var time;
+var maxSpeed;
+var distance;
 
-var calculateTime = function (dist) {
-    dist *= lightYear;
-    var t = Math.sqrt(dist/acceleration)*2/year;
-    time.text(t);
+var distanceInput = $("#input1");
+
+var timeOutput = $("#output1");
+var maxSpeedOutput = $("#output2");
+var maxRelativisticSpeed = $("#output3");
+
+var calculateTime = function () {
+    var dist = distance * lightYear;
+    time = Math.sqrt(dist/acceleration)*2/year;
+    timeOutput.text("Време: " + time + " години");
 };
 
-distance.change(function () {
-    var distanceValue = distance.val();
-    calculateTime(distanceValue);
+var calculateMaxSpeed = function () {
+    maxSpeed = time*acceleration*year/2;
+    maxSpeedOutput.text("Максимална скорост: " + (maxSpeed/1000) + " m/s");
+    maxRelativisticSpeed.text("Максимална скорост: " + maxSpeed/speedOfLight + " *c");
+};
+
+distanceInput.change(function () {
+    distance= distanceInput.val();
+    calculateTime();
+    calculateMaxSpeed();
 });
